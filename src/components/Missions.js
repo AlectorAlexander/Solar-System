5e1f60cf31de68a839e0cbf99bd53c0cde7eedcc
Aléctor Alexander <alector.alexandre@gmail.com>
2022-02-20 14:23:24 -0300

    Meu primeiro projetinho React pronto *-*

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ol>
+          {missions.map((element) => (<MissionCard
+            key={ element.name }
+            name={ element.name }
+            year={ JSON.parse(element.year) }
+            country={ element.country }
+            destination={ element.destination }
+          />))}
+        </ol>
       </div>
     );
   }
